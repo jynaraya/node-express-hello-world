c65e6e5152c4848a6e7d39f4294fdc74e99d50e7
jynaraya <143063932+jynaraya@users.noreply.github.com>
2025-03-07 15:58:36 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,3 @@
-exports.home = function(req, res) {
-    res.render('home'); // Render home.ejs for the homepage
-};
 
 // Render home.ejs as a maintenance page for /login
 exports.maintenance = function(req, res) {
