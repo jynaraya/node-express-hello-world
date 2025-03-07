50d17b88abb04904134d023e42c46e4385ed8a25
jynaraya <143063932+jynaraya@users.noreply.github.com>
2025-03-07 15:49:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 var express = require('express');
 var app = express();
 
-var routes = require('./routes/route.js');
-
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', routes.home);
-app.get('/login', routes.maintenance); // Show home.ejs on /login
+// Middleware to show maintenance page for all routes
+app.use((req, res, next) => {
+    res.render('home'); // Renders home.ejs for all requests
+});
 
 var port = process.env.PORT || 3000;
 
 var server = app.listen(port, function() {
     console.log("Catch the action at http://localhost:" + port);
 });
-
